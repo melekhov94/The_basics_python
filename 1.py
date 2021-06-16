552f73684bd29938d45533c68b9786809fbd5d5d
Vycheslav <melekhov9445@gmail.com>
2021-06-17 02:43:12 +0500

    dz

--- 1.py
+++
@@ -1,7 +0,0 @@
-sec = int(input("Введите секунды: "))
-a = int(sec // 3600)
-b = int((sec % 3600) // 60)
-c = int((sec % 3600) % 60)
-result = ["{} дней {} минут {} секунд".format(a, b, c)]
-
-print(result)
